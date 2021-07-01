f49ea23e0923c030b90b026ab3f0a8d74cde7ca0
Briana Breaux <makeitbreaux@gmail.com>
2021-06-30 21:11:10 -0400

    Python Syntax Exercise

---
+++ words.py
@@ -0,0 +1,22 @@
+def print_upper_words(words):
+    """Print each word on a separate line, uppercased."""
+    for word in words:
+        print(word.upper())
+        
+        
+        
+def print_upper_words2(words):
+    """Print each word on separate line, uppercased, if starts with E or e."""
+    for word in words:
+        if word.startswith("e") or word.startswith("E"):
+            print(word.upper())
+            
+            
+            
+def print_upper_words3(words, must_start_with):
+    """Print each word on separate line, uppercased, if starts with one of given"""
+    for word in words:
+        for letter in must_start_with:
+            if word.startswith(letter):
+                print(word.upper())
+                break
